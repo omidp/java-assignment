8f110f0965aec840e8ce8a9b2618a596ff0adbe0
shuwen.lim <shuwen.lim@fwd.com>
2022-04-21 23:43:28 +0800

    -[NEW] iteration 9

--- src/main/java/com/kata/wordcount/Application.java
+++ src/main/java/com/kata/wordcount/Application.java
@@ -8,33 +8,43 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import org.apache.commons.lang3.StringUtils;
+
 
 public class Application {
 	public static void main(String[] args) throws IOException {
-		boolean isIndex = true;
+		boolean isIndex = false;
 		
 		try (Scanner sc = new Scanner(System.in)) {
-			System.out.println("Enter text:");
 			Path path = Paths.get("src/main/resources/dictionary.txt");
 
-			List<String> filteredWords = CommonUtils.getFilteredWords(sc.nextLine());
-			List<String> uniqueWords = CommonUtils.getUniqueWords(filteredWords);
-
-
-			System.out.println("Number of words: " + filteredWords.size() + ", unique: " + uniqueWords.size() 
-			+ "; average word length: " + CommonUtils.getAverageWordLength(filteredWords) + " characters");
-
-			if(isIndex && Files.exists(path)) {
-				System.out.println("here:");
-				List<String> dictWords = Files.readAllLines(path);
-				Map<String, Object> updatedMap = CommonUtils.markUnknownWords(filteredWords, dictWords);
+			while(true) {
+				System.out.println("Enter text:");
+				String input = sc.nextLine();
 				
-			
-				List<String> updatedFilteredWords = (List<String>) updatedMap.get("filteredWords");
-				
-				System.out.println("Index (unknown: " + updatedMap.get("count") + "): ");
-				for(String str : updatedFilteredWords) {
-					System.out.println(str);
+				if(!StringUtils.isBlank(input)) {
+					List<String> filteredWords = CommonUtils.getFilteredWords(input);
+					List<String> uniqueWords = CommonUtils.getUniqueWords(filteredWords);
+
+
+					System.out.println("Number of words: " + filteredWords.size() + ", unique: " + uniqueWords.size() 
+					+ "; average word length: " + CommonUtils.getAverageWordLength(filteredWords) + " characters");
+
+					if(isIndex && Files.exists(path)) {
+						System.out.println("here:");
+						List<String> dictWords = Files.readAllLines(path);
+						Map<String, Object> updatedMap = CommonUtils.markUnknownWords(filteredWords, dictWords);
+						
+					
+						List<String> updatedFilteredWords = (List<String>) updatedMap.get("filteredWords");
+						
+						System.out.println("Index (unknown: " + updatedMap.get("count") + "): ");
+						for(String str : updatedFilteredWords) {
+							System.out.println(str);
+						}
+					}
+				}else {
+					break;
 				}
 			}
 		}
